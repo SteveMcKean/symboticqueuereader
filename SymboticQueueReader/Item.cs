d08209d884f5551ba57c01e0324ae05edf55f22f
Steve McKean <steven.mckean@peraton.com>
2024-07-31 14:03:53 -0500

    property updates.

--- SymboticQueueReader/Item.cs
+++ SymboticQueueReader/Item.cs
@@ -8,17 +8,22 @@ using Newtonsoft.Json;
 
 public class Item
 {
+    public Item(List<InboundDimensionalVariant> inboundDimensionalVariants)
+    {
+        InboundDimensionalVariants = inboundDimensionalVariants;
+    }
+
     [JsonProperty(propertyName: nameof(RequestId))]
     public Guid RequestId { get; set; }
 
     [JsonProperty(propertyName: nameof(SkuId))]
     public Guid SkuId { get; set; }
 
-    [JsonProperty(propertyName: "Sku")]
+    [JsonProperty(propertyName: nameof(Sku))]
     public string? Sku { get; set; }
 
     [JsonProperty(propertyName: nameof(Description))]
-    public string Description { get; set; }
+    public string? Description { get; set; }
 
     [JsonProperty(propertyName: nameof(IsPressurized))]
     public bool IsPressurized { get; set; }
@@ -26,48 +31,48 @@ public class Item
     [JsonProperty(propertyName: nameof(IsEligible))]
     public bool IsEligible { get; set; }
 
-    [JsonProperty(propertyName: "IsFridgePack")]
+    [JsonProperty(propertyName: nameof(IsFridgePack))]
     public bool IsFridgePack { get; set; }
 
-    [JsonProperty(propertyName: "LayersPerPallet")]
+    [JsonProperty(propertyName: nameof(LayersPerPallet))]
     public int LayersPerPallet { get; set; }
 
-    [JsonProperty(propertyName: "CasesPerLayer")]
+    [JsonProperty(propertyName: nameof(CasesPerLayer))]
     public int CasesPerLayer { get; set; }
 
-    [JsonProperty(propertyName: "AllergenTypeId")]
-    public string AllergenTypeId { get; set; }
+    [JsonProperty(propertyName: nameof(AllergenTypeId))]
+    public string? AllergenTypeId { get; set; }
 
-    [JsonProperty(propertyName: "PackagingTypeId")]
+    [JsonProperty(propertyName: nameof(PackagingTypeId))]
     public int PackagingTypeId { get; set; }
 
-    [JsonProperty(propertyName: "PalletizationGroupTypeId")]
+    [JsonProperty(propertyName: nameof(PalletizationGroupTypeId))]
     public int PalletizationGroupTypeId { get; set; }
 
-    [JsonProperty(propertyName: "IsFragile")]
+    [JsonProperty(propertyName: nameof(IsFragile))]
     public bool IsFragile { get; set; }
 
-    [JsonProperty(propertyName: "SegregationCode")]
+    [JsonProperty(propertyName: nameof(SegregationCode))]
     public int SegregationCode { get; set; }
 
-    [JsonProperty(propertyName: "ProductTypes")]
-    public string ProductTypes { get; set; }
+    [JsonProperty(propertyName: nameof(ProductTypes))]
+    public string? ProductTypes { get; set; }
 
-    [JsonProperty(propertyName: "GripForce")]
+    [JsonProperty(propertyName: nameof(GripForce))]
     public double GripForce { get; set; }
 
-    [JsonProperty(propertyName: "IsAutoDepal")]
+    [JsonProperty(propertyName: nameof(IsAutoDepal))]
     public bool IsAutoDepal { get; set; }
 
-    [JsonProperty(propertyName: "InboundDimensionalVariants")]
+    [JsonProperty(propertyName: nameof(InboundDimensionalVariants))]
     public List<InboundDimensionalVariant> InboundDimensionalVariants { get; set; }
 
-    [JsonProperty(propertyName: "Extensions")]
-    public Dictionary<string, string> Extensions { get; set; }
+    [JsonProperty(propertyName: nameof(Extensions))]
+    public Dictionary<string, string>? Extensions { get; set; }
 
-    [JsonProperty(propertyName: "IsBreakpackEligibleOverride")]
+    [JsonProperty(propertyName: nameof(IsBreakpackEligibleOverride))]
     public bool? IsBreakpackEligibleOverride { get; set; }
 
-    [JsonProperty(propertyName: "WarehousePack")]
-    public WarehousePack WarehousePack { get; set; }
+    [JsonProperty(propertyName: nameof(WarehousePack))]
+    public WarehousePack? WarehousePack { get; set; }
 }
